804e9ff202533eacc578f68bc435c4946315101a
office-bsmx <62848374+office-bsmx@users.noreply.github.com>
2020-05-07 01:24:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+VK.init(function() {
+     // API initialization succeeded
+     // Your code here
+  }, function() {
+     // API initialization failed
+     // Can reload page here
+}, '5.103');
+VK.api("user.get", {"fields": "photo50, city", "v":"5.103"}, function (data) {
 
+    console.log(data.response[o]); 
+
+});
